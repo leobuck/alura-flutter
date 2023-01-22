bda6fe20d119fd8f2020508ae60aeb51318f0330
leobuck <leobuck12@gmail.com>
2023-01-22 19:51:51 -0300

    Refatorando Difficulty

--- primeiro_projeto/lib/main.dart
+++ primeiro_projeto/lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:primeiro_projeto/difficulty.dart';
 
 void main() {
   runApp(MyApp());
@@ -132,44 +133,8 @@ class _TaskState extends State<Task> {
                                   overflow: TextOverflow.ellipsis),
                             ),
                           ),
-                          Row(
-                            children: [
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: widget.dificuldade >= 1
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: widget.dificuldade >= 2
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: widget.dificuldade >= 3
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: widget.dificuldade >= 4
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                              Icon(
-                                Icons.star,
-                                size: 15,
-                                color: widget.dificuldade >= 5
-                                    ? Colors.blue
-                                    : Colors.blue[100],
-                              ),
-                            ],
+                          Difficulty(
+                            difficultyLevel: widget.dificuldade,
                           ),
                         ],
                       ),
