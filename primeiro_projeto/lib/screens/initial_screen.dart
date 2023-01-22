4cb58aeabd93018ced8fdf394ce70610780908f4
leobuck <leobuck12@gmail.com>
2023-01-22 20:43:52 -0300

    Assets

--- primeiro_projeto/lib/screens/initial_screen.dart
+++ primeiro_projeto/lib/screens/initial_screen.dart
@@ -23,24 +23,14 @@ class _InitialScreenState extends State<InitialScreen> {
         duration: const Duration(milliseconds: 800),
         child: ListView(
           children: const [
-            Task(
-                'Aprender Flutter',
-                'https://pbs.twimg.com/media/Eu7m692XIAEvxxP?format=png&name=large',
-                3),
-            Task(
-                'Andar de bike',
-                'https://tswbike.com/wp-content/uploads/2020/09/108034687_626160478000800_2490880540739582681_n-e1600200953343.jpg',
-                2),
-            Task(
-                'Meditar',
-                'https://manhattanmentalhealthcounseling.com/wp-content/uploads/2019/06/Top-5-Scientific-Findings-on-MeditationMindfulness-881x710.jpeg',
-                5),
-            Task(
-                'Ler',
-                'https://thebogotapost.com/wp-content/uploads/2017/06/636052464065850579-137719760_flyer-image-1.jpg',
-                4),
-            Task('Jogar',
-                'https://i.ibb.co/tB29PZB/kako-epifania-2022-2-c-pia.jpg', 1),
+            Task('Aprender Flutter', 'assets/images/dash.png', 3),
+            Task('Andar de bike', 'assets/images/bike.webp', 2),
+            Task('Meditar', 'assets/images/meditar.jpeg', 5),
+            Task('Ler', 'assets/images/livro.jpg', 4),
+            Task('Jogar', 'assets/images/jogar.jpg', 1),
+            SizedBox(
+              height: 80,
+            )
           ],
         ),
       ),
