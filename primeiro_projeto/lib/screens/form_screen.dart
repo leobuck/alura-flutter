01908f9194e92296f0302fd8e5bea448441d5fcf
leobuck <leobuck12@gmail.com>
2023-01-30 21:24:45 -0300

    ErroBuilder

--- primeiro_projeto/lib/screens/form_screen.dart
+++ primeiro_projeto/lib/screens/form_screen.dart
@@ -28,6 +28,8 @@ class _FormScreenState extends State<FormScreen> {
             border: Border.all(width: 3),
           ),
           child: Column(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            crossAxisAlignment: CrossAxisAlignment.center,
             children: [
               Padding(
                 padding: const EdgeInsets.all(8.0),
@@ -83,6 +85,10 @@ class _FormScreenState extends State<FormScreen> {
                   borderRadius: BorderRadius.circular(10),
                   child: Image.network(
                     imageController.text,
+                    errorBuilder: (BuildContext context, Object exception,
+                        StackTrace? stackTrace) {
+                      return Image.asset('assets/images/nophoto.png');
+                    },
                     fit: BoxFit.cover,
                   ),
                 ),
