7ca163661e91e7ca61000d5cc32bcadc2eca625a
leobuck <leobuck12@gmail.com>
2023-03-01 21:33:48 -0300

    Novo Widget: Inherited Widget

---
+++ primeiro_projeto/lib/data/task_inherited.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'package:primeiro_projeto/components/task.dart';
+
+class TaskInherited extends InheritedWidget {
+  TaskInherited({super.key, required this.child}) : super(child: child);
+
+  final Widget child;
+  final List<Task> taskList = [
+    Task('Aprender Flutter', 'assets/images/dash.png', 3),
+    Task('Andar de bike', 'assets/images/bike.webp', 2),
+    Task('Meditar', 'assets/images/meditar.jpeg', 5),
+    Task('Ler', 'assets/images/livro.jpg', 4),
+    Task('Jogar', 'assets/images/jogar.jpg', 1),
+  ];
+
+  void newTask(String name, String photo, int difficulty) {
+    taskList.add(Task(name, photo, difficulty));
+  }
+
+  static TaskInherited? of(BuildContext context) {
+    return context.dependOnInheritedWidgetOfExactType<TaskInherited>();
+  }
+
+  @override
+  bool updateShouldNotify(TaskInherited oldWidget) {
+    return true;
+  }
+}
