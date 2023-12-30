146467744d2678b95741252b5306258912d37b4f
leobuck <leobuck12@gmail.com>
2023-12-30 13:41:43 -0300

    Produzindo o ContentDivision

--- alubank/lib/screens/home.dart
+++ alubank/lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:alubank/components/box_card.dart';
 import 'package:alubank/components/color_dot.dart';
+import 'package:alubank/components/content_division.dart';
 import 'package:alubank/components/sections/header.dart';
 import 'package:alubank/themes/theme_colors.dart';
 import 'package:flutter/material.dart';
@@ -14,7 +15,7 @@ class Home extends StatelessWidget {
         children: <Widget>[
           Header(),
           BoxCard(
-            boxContent: ColorDot(color: ThemeColors.recentActivity['spent']),
+            boxContent: ContentDivision(),
           )
         ],
       ),
