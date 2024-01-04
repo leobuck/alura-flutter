a5fc697dbb44d84b86caa8c7efc5759657e99b81
leobuck <leobuck12@gmail.com>
2024-01-03 21:20:09 -0300

    Definindo o RecentActivity

--- alubank/lib/screens/home.dart
+++ alubank/lib/screens/home.dart
@@ -1,8 +1,5 @@
-import 'package:alubank/components/box_card.dart';
-import 'package:alubank/components/color_dot.dart';
-import 'package:alubank/components/content_division.dart';
 import 'package:alubank/components/sections/header.dart';
-import 'package:alubank/themes/theme_colors.dart';
+import 'package:alubank/components/sections/recent_activity.dart';
 import 'package:flutter/material.dart';
 
 class Home extends StatelessWidget {
@@ -14,9 +11,7 @@ class Home extends StatelessWidget {
       body: Column(
         children: <Widget>[
           Header(),
-          BoxCard(
-            boxContent: ContentDivision(),
-          )
+          RecentActivity(),
         ],
       ),
     );
