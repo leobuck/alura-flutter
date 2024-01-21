7b576d9cc205ced0865f88ed06f91f09fa8d0887
leobuck <leobuck12@gmail.com>
2024-01-21 17:56:44 -0300

    Resposta do desafio

--- panucci_ristorante/lib/screens/food_menu.dart
+++ panucci_ristorante/lib/screens/food_menu.dart
@@ -1,10 +1,43 @@
 import 'package:flutter/material.dart';
+import 'package:panucci_ristorante/cardapio.dart';
+import 'package:panucci_ristorante/components/food_item.dart';
 
 class FoodMenu extends StatelessWidget {
-const FoodMenu({ Key? key }) : super(key: key);
+  const FoodMenu({Key? key}) : super(key: key);
+  final List items = comidas;
 
   @override
-  Widget build(BuildContext context){
-    return Container();
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.fromLTRB(16.0, 16.0, 16.0, 0.0),
+      child: CustomScrollView(
+        slivers: <Widget>[
+          const SliverToBoxAdapter(
+            child: Padding(
+              padding: EdgeInsets.only(bottom: 16.0),
+              child: Text(
+                "Menu",
+                style: TextStyle(
+                  fontFamily: "Caveat",
+                  fontSize: 32,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
+                return FoodItem(
+                    itemTitle: items[index]["name"],
+                    itemPrice: items[index]["price"],
+                    imageURI: items[index]["image"]);
+              },
+              childCount: items.length,
+            ),
+          ),
+        ],
+      ),
+    );
   }
 }
