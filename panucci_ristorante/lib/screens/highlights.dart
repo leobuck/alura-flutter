aec6aeed484634ead8d47c918c63b3fc9860315c
leobuck <leobuck12@gmail.com>
2024-01-21 16:54:24 -0300

    Aplicando o CustomScrollView

--- panucci_ristorante/lib/screens/highlights.dart
+++ panucci_ristorante/lib/screens/highlights.dart
@@ -10,20 +10,21 @@ class Highlights extends StatelessWidget {
   Widget build(BuildContext context) {
     return Padding(
       padding: const EdgeInsets.all(16.0),
-      child: Column(
-        children: [
-          Text("Destaques"),
-          SizedBox(
-            height: MediaQuery.of(context).size.height,
-            child: ListView.builder(
-              itemBuilder: (context, index) {
+      child: CustomScrollView(
+        slivers: <Widget>[
+          SliverToBoxAdapter(
+            child: Text("Destaques"),
+          ),
+          SliverList(
+            delegate: SliverChildBuilderDelegate(
+              (context, index) {
                 return HighlightItem(
                     imageURI: items[index]["image"],
                     itemTitle: items[index]["name"],
                     itemPrice: items[index]["price"],
                     itemDescription: items[index]["description"]);
               },
-              itemCount: items.length,
+              childCount: items.length,
             ),
           ),
         ],
