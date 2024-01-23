466755d88bb793fa94d1d9bd2ff204ac6987a79e
leobuck <leobuck12@gmail.com>
2024-01-23 10:38:56 -0300

    Completando a página de checkout com o Expanded

--- panucci_ristorante/lib/screens/checkout.dart
+++ panucci_ristorante/lib/screens/checkout.dart
@@ -2,6 +2,8 @@ import 'package:flutter/material.dart';
 import 'package:panucci_ristorante/cardapio.dart';
 import 'package:panucci_ristorante/components/main_drawer.dart';
 import 'package:panucci_ristorante/components/order_item.dart';
+import 'package:panucci_ristorante/components/payment_method.dart';
+import 'package:panucci_ristorante/components/payment_total.dart';
 
 class Checkout extends StatelessWidget {
   const Checkout({Key? key}) : super(key: key);
@@ -29,9 +31,9 @@ class Checkout extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: CustomScrollView(
           slivers: <Widget>[
-            SliverToBoxAdapter(
+            const SliverToBoxAdapter(
               child: Padding(
-                padding: const EdgeInsets.only(bottom: 8.0),
+                padding: EdgeInsets.only(bottom: 8.0),
                 child: Text(
                   "Pedido",
                   style: TextStyle(
@@ -51,6 +53,36 @@ class Checkout extends StatelessWidget {
                 },
                 childCount: items.length,
               ),
+            ),
+            const SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  "Pagamento",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            const SliverToBoxAdapter(
+              child: PaymentMethod(),
+            ),
+            const SliverToBoxAdapter(
+              child: Padding(
+                padding: EdgeInsets.only(bottom: 8.0),
+                child: Text(
+                  "Confirmar",
+                  style: TextStyle(
+                    fontSize: 24,
+                    fontWeight: FontWeight.w600,
+                  ),
+                ),
+              ),
+            ),
+            const SliverToBoxAdapter(
+              child: PaymentTotal(),
             ),
           ],
         ),
