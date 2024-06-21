eedf8a349924a94302c5d9e19229189d419f0367
leobuck <leobuck12@gmail.com>
2024-06-20 23:04:49 -0300

    Enviando o dispositivo para o servidor

--- flutter-notifications/lib/http/web.dart
+++ flutter-notifications/lib/http/web.dart
@@ -1,4 +1,5 @@
 import 'dart:convert';
+import 'package:meetups/models/device.dart';
 import 'package:meetups/models/event.dart';
 import 'package:http/http.dart' as http;
 
@@ -13,4 +14,22 @@ Future<List<Event>> getAllEvents() async {
   } else {
     throw Exception('Falha ao carregar os eventos');
   }
+}
+
+void sendDevice(Device device) async {
+  final response = await http.post(
+    Uri.parse('$baseUrl/devices'),
+    headers: <String, String>{
+      'Content-Type': 'application/json; charset=UTF-8'
+    },
+    body: jsonEncode(
+      <String, String>{
+        'token': device.token ?? '',
+        'model': device.model ?? '',
+        'marca': device.brand ?? '',
+      },
+    ),
+  );
+
+  // return response;
 }
--- flutter-notifications/lib/main.dart
+++ flutter-notifications/lib/main.dart
@@ -1,7 +1,20 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_messaging/firebase_messaging.dart';
 import 'package:meetups/screens/events_screen.dart';
 import 'package:flutter/material.dart';
 
-void main() => runApp(App());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  FirebaseMessaging messaging = FirebaseMessaging.instance;
+
+  String? token = await messaging.getToken();
+  print('TOKEN: $token');
+  setPushToken(token);
+  runApp(App());
+}
+
+void setPushToken(String? token) async {}
 
 class App extends StatelessWidget {
   @override
