ad56bae654de933ab3de883240ff13a0f359931c
leobuck <leobuck12@gmail.com>
2024-11-17 15:52:32 -0300

    Utilizando interceptadores

--- flutter_webapi/lib/services/journal_service.dart
+++ flutter_webapi/lib/services/journal_service.dart
@@ -1,17 +1,22 @@
 import 'dart:convert';
 
+import 'package:flutter_webapi_first_course/services/http_interceptors.dart';
 import 'package:http/http.dart' as http;
+import 'package:http_interceptor/http/http.dart';
 
 class JournalService {
   static const String url = "http://192.168.0.50:3000/";
   static const String resource = "learnhttp";
+
+  http.Client client =
+      InterceptedClient.build(interceptors: [LoggingInterceptor()]);
 
   String getUrl() {
     return "$url$resource";
   }
 
   register(String content) {
-    http.post(
+    client.post(
       Uri.parse(getUrl()),
       headers: {"Content-Type": "application/json"},
       body: json.encode({"content": content}),
@@ -19,7 +24,7 @@ class JournalService {
   }
 
   Future<String> get() async {
-    http.Response response = await http.get(
+    http.Response response = await client.get(
       Uri.parse(getUrl()),
       headers: {"Content-Type": "application/json"},
     );
